630fb77980862af95d790a27249263f5c0f34493
sachipatel <sachixl300@gmail.com>
2026-04-24 14:11:42 +0530

    backend port change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://sachipatel-farm-advisory-back.onrender.com/api",
 });
 
-API.interceptors.request.use(req => {
+API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
